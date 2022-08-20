1770ec4a8faaba73cac4a3beab3ac5c2dab99f16
Sadegh <sadeghjfr22@gmail.com>
2022-08-20 12:46:55 +0430

    setup package base

---
+++ app/src/main/java/com/myapp/myshop/base/App.kt
@@ -0,0 +1,6 @@
+package com.myapp.myshop.base
+
+import android.app.Application
+
+class App: Application() {
+}
---
+++ app/src/main/java/com/myapp/myshop/base/ViewModelFactory.kt
@@ -0,0 +1,22 @@
+package com.myapp.myshop.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+
+class ViewModelFactory : ViewModelProvider.Factory {
+
+    //private val mainRepo: MainRepo? = null
+
+    /*fun ViewModelFactory(mainRepo: MainRepo) {
+        mainRepo = mainRepo
+    }*/
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+
+       /* if (modelClass.name.equals(MainViewModel::class.java.getName()))
+            return MainViewModel(mainRepo,) as T
+
+        return null*/
+    }
+}
