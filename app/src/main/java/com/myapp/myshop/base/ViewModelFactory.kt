4d285f7e2a732c2cb3e2f90dd1b82057667e7829
Sadegh <sadeghjfr22@gmail.com>
2022-08-22 13:05:22 +0430

    bottom navigation menu added.

--- app/src/main/java/com/myapp/myshop/base/ViewModelFactory.kt
+++ app/src/main/java/com/myapp/myshop/base/ViewModelFactory.kt
@@ -1,3 +1,4 @@
+/*
 package com.myapp.myshop.base
 
 import androidx.lifecycle.ViewModel
@@ -8,15 +9,19 @@ class ViewModelFactory : ViewModelProvider.Factory {
 
     //private val mainRepo: MainRepo? = null
 
-    /*fun ViewModelFactory(mainRepo: MainRepo) {
+    */
+/*fun ViewModelFactory(mainRepo: MainRepo) {
         mainRepo = mainRepo
-    }*/
+    }*//*
+
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
 
-       /* if (modelClass.name.equals(MainViewModel::class.java.getName()))
+       */
+/* if (modelClass.name.equals(MainViewModel::class.java.getName()))
             return MainViewModel(mainRepo,) as T
 
-        return null*/
+        return null*//*
+
     }
-}
+}*/
--- app/src/main/java/com/myapp/myshop/ui/view/activity/MainActivity.kt
+++ app/src/main/java/com/myapp/myshop/ui/view/activity/MainActivity.kt
@@ -2,11 +2,27 @@ package com.myapp.myshop.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.myapp.myshop.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation(){
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        NavigationUI.setupWithNavController(bottomNav, navHostFragment.navController)
     }
 }
