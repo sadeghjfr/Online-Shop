fa07328bf71cfb4ecf4d82d8c72e7d87a6bb1e5a
Sadegh <sadeghjfr22@gmail.com>
2022-08-22 14:08:47 +0430

    CategoryFragment changed to DiscoverFragment.

--- app/src/main/java/com/myapp/myshop/ui/view/fragment/DiscoverFragment.kt
+++ app/src/main/java/com/myapp/myshop/ui/view/fragment/DiscoverFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.myapp.myshop.R
 
-class CategoryFragment : Fragment() {
+class DiscoverFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater,
                               container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_category, container, false)
+        return inflater.inflate(R.layout.fragment_discover, container, false)
     }
 
 }
